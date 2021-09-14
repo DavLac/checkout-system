4e41d106915c94dccc96f5e333d18692ff799991
David.Lacoste <david.lacoste2@gmail.com>
2021-09-14 23:25:21 +0200

    refactor : remove basic error controller from swagger

--- src/main/java/io/davlac/checkoutsystem/config/ApplicationConfig.java
+++ src/main/java/io/davlac/checkoutsystem/config/ApplicationConfig.java
@@ -1,9 +1,23 @@
 package io.davlac.checkoutsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableJpaAuditing
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public Docket demoApi() {
+        // remove basic error controller from swagger
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.davlac.checkoutsystem"))
+                .build();
+    }
+
 }
